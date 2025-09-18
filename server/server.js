f5ca140a97b7d650ea9b8670e18ef9d1199955fd
victor Muriithi <victornjiru80@gmail.com>
2025-09-18 13:14:50 +0300

    cors update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000', 'http://localhost:5173'];
+const allowedOrigins = [process.env.FRONTEND_URL, ' https://puzzle-paradise.onrender.com', 'http://localhost:5173'];
 
 app.use(cors({
     origin: function(origin, callback){
